2b03fcf2f7d0df1402fc144ab98baf8c2e89853d
andysolomon <andrewsolomon.edu@gmail.com>
2023-02-17 13:25:58 -0500

    Initial Commit

---
+++ src/api/pricing-tier/controllers/pricing-tier.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * pricing-tier controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::pricing-tier.pricing-tier');
---
+++ src/api/pricing-tier/routes/pricing-tier.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * pricing-tier router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::pricing-tier.pricing-tier');
